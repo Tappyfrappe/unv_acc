dc32c34ad10f30bbd6b2df9df60a33ef4d743845
Tappyfrappe <ladymelodiluzgalope@gmail.com>
2024-12-14 19:02:48 +0800

    Update room_assignments.php

--- pages/room_assignments.php
+++ pages/room_assignments.php
@@ -1,15 +1,64 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+
+    .mra-container {
+        text-align: center;
+    }
+    .navbar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #f9f9f9;
+    }
+
+    .navbar nav {
+        background-color: #f1485b;
+        padding: 10px 20px;
+        border-radius: 8px;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
+    }
+
+    .navbar nav a {
+        text-decoration: none;
+        color: white;
+        transition: all 0.3s ease;
+    }
+
+    .navbar nav a:hover {
+        text-decoration: none;
+        color: #212f3c;
+    }
+</style>
+
+
+<body>
 <?php
 include '../includes/db_connect.php';
 include '../includes/header.php';
 ?>
 
-<h2>Manage Room Assignments</h2>
+<h2 class="mra-container">Manage Room Assignments</h2>
 
 <!-- Tabs for Halls and Flats -->
-<nav>
+ <div class="navbar">
+    <nav>
     <a href="?type=hall">Assign Rooms in Halls</a> |
     <a href="?type=flat">Assign Rooms in Flats</a>
 </nav>
+ </div>
+
 
 <?php
 $type = $_GET['type'] ?? 'hall'; // Default to halls
@@ -21,3 +70,8 @@ if ($type === 'hall') {
 ?>
 
 <?php include '../includes/footer.php'; ?>
+
+</body>
+</html>
+
+
