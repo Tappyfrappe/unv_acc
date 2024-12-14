18d903cd7719bca39472ae61f74c3efd897a73d5
Rei0021 <zarhannaabdulharid@gmail.com>
2024-12-14 21:16:31 +0800

    fix logout

--- pages/leases.php
+++ pages/leases.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: ../pages/login.php");
+    exit();
+}
+?>
+
+<?php
 include '../includes/db_connect.php';
 include '../includes/header.php';
 ?>
